524d6177922f51f0e821eec933c6184386d03836
Jintao_Qu <00002@protonmail.com>
2018-12-14 13:26:30 +0800

    test change lable

---
+++ test/test1.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+cnt = 0
+def myLabel(event):
+    global root
+    s = tk.Label(root, text="Hello world")
+    s.pack()
+def change(event):
+    global cnt
+    cnt += 1
+    text.set(str(cnt))
+
+
+root = tk.Tk()
+B = tk.Button(root, text="点击")
+B.bind("<Button-1>", change)
+text = tk.StringVar()
+text.set('0')
+S = tk.Label(root, text="Hello world")
+B.pack(side=tk.LEFT)
+S.pack(side=tk.RIGHT)
+root.mainloop()
